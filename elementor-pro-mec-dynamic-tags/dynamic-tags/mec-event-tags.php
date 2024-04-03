a13a2f9bf54974162862ab484fb4ecaec7460de6
Tomáš Biheler <tomas.biheler@impacthub.cz>
2024-04-03 18:20:45 +0200

    Initial working commit

---
+++ elementor-pro-mec-dynamic-tags/dynamic-tags/mec-event-tags.php
@@ -0,0 +1,264 @@
+<?php
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+class Elementor_Dynamic_Tag_MEC_Event_Name extends \Elementor\Core\DynamicTags\Tag
+{
+    public function get_name()
+    {
+        return 'mec-event-name';
+    }
+
+    public function get_title()
+    {
+        return 'Event Name';
+    }
+
+    public function get_group()
+    {
+        return 'mec-event';
+    }
+
+    public function get_categories()
+    {
+        return ['text'];
+    }
+
+    public function render()
+    {
+        global $post;
+        if (is_singular(['mec-events', 'mec_esb'])) {
+            $eventID = $post->ID;
+            echo $post->post_title;
+        } else {
+            return;
+        }
+    }
+}
+
+
+class Elementor_Dynamic_Tag_MEC_Event_Location_Name extends \Elementor\Core\DynamicTags\Tag
+{
+    public function get_name()
+    {
+        return 'mec-event-location-name';
+    }
+
+    public function get_title()
+    {
+        return 'Location Name';
+    }
+
+    public function get_group()
+    {
+        return 'mec-event';
+    }
+
+    public function get_categories()
+    {
+        return ['text'];
+    }
+    public function render()
+    {
+        global $post;
+        if (is_singular(['mec-events', 'mec_esb'])) { // Check if it's a single event page.
+            $eventID = $post->ID; // This is your event ID.
+            $locationTermId = get_post_meta($eventID, 'mec_location_id', true);
+            $locationTerm = get_term($locationTermId, 'mec_location');
+            echo $locationTerm->name;
+        } else {
+            return;
+        }
+    }
+}
+
+class Elementor_Dynamic_Tag_MEC_Event_Location_Address extends \Elementor\Core\DynamicTags\Tag
+{
+    public function get_name()
+    {
+        return 'mec-event-location-address';
+    }
+
+    public function get_title()
+    {
+        return 'Location Address';
+    }
+
+    public function get_group()
+    {
+        return 'mec-event';
+    }
+
+    public function get_categories()
+    {
+        return ['text'];
+    }
+    public function render()
+    {
+        global $post;
+        if (is_singular(['mec-events', 'mec_esb'])) {
+            $eventID = $post->ID;
+            $locationTermId = get_post_meta($eventID, 'mec_location_id', true);
+            $addressTermMeta = get_term_meta($locationTermId, 'address', true);
+            echo $addressTermMeta;
+        } else {
+            return;
+        }
+    }
+}
+class Elementor_Dynamic_Tag_MEC_Event_Organizer_Name extends \Elementor\Core\DynamicTags\Tag
+{
+    public function get_name()
+    {
+        return 'mec-event-organizer-name';
+    }
+    public function get_title()
+    {
+        return 'Organizer Name';
+    }
+    public function get_group()
+    {
+        return 'mec-event';
+    }
+    public function get_categories()
+    {
+        return ['text'];
+    }
+    public function render()
+    {
+        global $post;
+        if (is_singular(['mec-events', 'mec_esb'])) {
+            $organizerTermId = get_post_meta($post->ID, 'mec_organizer_id', true);
+            $organizerTerm = get_term($organizerTermId);
+            echo $organizerTerm->name;
+        } else {
+            return;
+        }
+    }
+}
+class Elementor_Dynamic_Tag_MEC_Event_Organizer_Website extends \Elementor\Core\DynamicTags\Tag
+{
+    public function get_name()
+    {
+        return 'mec-event-organizer-website';
+    }
+    public function get_title()
+    {
+        return 'Organizer Website';
+    }
+    public function get_group()
+    {
+        return 'mec-event';
+    }
+    public function get_categories()
+    {
+        return ['text', 'url'];
+    }
+    public function render()
+    {
+        global $post;
+        if (is_singular(['mec-events', 'mec_esb'])) {
+            $organizerTermId = get_post_meta($post->ID, 'mec_organizer_id', true);
+            $website = get_term_meta($organizerTermId, 'url', true);
+            echo $website;
+        } else {
+            return;
+        }
+    }
+}
+class Elementor_Dynamic_Tag_MEC_Event_Cost extends \Elementor\Core\DynamicTags\Tag
+{
+    public function get_name()
+    {
+        return 'mec-event-cost';
+    }
+    public function get_title()
+    {
+        return 'Event Cost';
+    }
+    public function get_group()
+    {
+        return 'mec-event';
+    }
+    public function get_categories()
+    {
+        return ['text'];
+    }
+    public function render()
+    {
+        global $post;
+        if (is_singular(['mec-events', 'mec_esb'])) {
+            $cost = get_post_meta($post->ID, 'mec_cost', true);
+            echo $cost;
+        } else {
+            return;
+        }
+    }
+}
+class Elementor_Dynamic_Tag_MEC_Event_Start_Date extends \Elementor\Core\DynamicTags\Tag
+{
+    public function get_name()
+    {
+        return 'mec-event-start-date';
+    }
+    public function get_title()
+    {
+        return 'Event Start Date';
+    }
+    public function get_group()
+    {
+        return 'mec-event';
+    }
+    public function get_categories()
+    {
+        return ['text'];
+    }
+    public function render()
+    {
+        global $post;
+        if (is_singular(['mec-events', 'mec_esb'])) {
+            global $wpdb;
+            $table_name = $wpdb->prefix . 'mec_events';
+            $query = $wpdb->prepare("SELECT start FROM $table_name WHERE post_id = %d", $post->ID);
+            $start_date = $wpdb->get_var($query);
+            $date_format = get_option('date_format');
+            echo date($date_format, strtotime($start_date));
+        } else {
+            return;
+        }
+    }
+}
+class Elementor_Dynamic_Tag_MEC_Event_End_Date extends \Elementor\Core\DynamicTags\Tag
+{
+    public function get_name()
+    {
+        return 'mec-event-end-date';
+    }
+    public function get_title()
+    {
+        return 'Event End Date';
+    }
+    public function get_group()
+    {
+        return 'mec-event';
+    }
+    public function get_categories()
+    {
+        return ['text'];
+    }
+    public function render()
+    {
+        global $post;
+        if (is_singular(['mec-events', 'mec_esb'])) {
+            global $wpdb;
+            $table_name = $wpdb->prefix . 'mec_events';
+            $query = $wpdb->prepare("SELECT end FROM $table_name WHERE post_id = %d", $post->ID);
+            $end_date = $wpdb->get_var($query);
+            $date_format = get_option('date_format');
+            echo date($date_format, strtotime($end_date));
+        } else {
+            return;
+        }
+    }
+}
---
+++ elementor-pro-mec-dynamic-tags/elementor-pro-mec-dynamic-tags.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Plugin Name: Elementor Pro - MEC Event Dynamic Tags
+ * Description: This plugin adds Elementor Pro Dynamic Tags for single MEC Event. Ideal for using ME Calendar to manage events and using Elementor to display them. Beware - This plugin overrides MEC single event template when active to allow for native Elementor Template.
+ * Version:     1.0.0
+ * Author:      Tomáš Biheler
+ * Author URI:  https://biheler.eu
+ *
+ * Requires Plugins: elementor,elementor-pro,modern-events-calendar-lite
+ * Elementor tested up to: 3.20.0
+ * Elementor Pro tested up to: 3.20.0
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+/**
+ * Register Dynamic Fields based on MEC event.
+ *
+ * Include dynamic tag file and register tag class.
+ *
+ * @since 1.0.0
+ * @param \Elementor\Core\DynamicTags\Manager $dynamic_tags_manager Elementor dynamic tags manager.
+ * @return void
+ */
+function elmedyta_register_mec_event_dynamic_tags($dynamic_tags_manager)
+{
+    require_once(__DIR__ . '/dynamic-tags/mec-event-tags.php');
+
+    $dynamic_tags_manager->register(new Elementor_Dynamic_Tag_MEC_Event_Name());
+    $dynamic_tags_manager->register(new Elementor_Dynamic_Tag_MEC_Event_Location_Name());
+    $dynamic_tags_manager->register(new Elementor_Dynamic_Tag_MEC_Event_Location_Address());
+    $dynamic_tags_manager->register(new Elementor_Dynamic_Tag_MEC_Event_Organizer_Name());
+    $dynamic_tags_manager->register(new Elementor_Dynamic_Tag_MEC_Event_Organizer_Website());
+    $dynamic_tags_manager->register(new Elementor_Dynamic_Tag_MEC_Event_Cost());
+    $dynamic_tags_manager->register(new Elementor_Dynamic_Tag_MEC_Event_Start_Date());
+    $dynamic_tags_manager->register(new Elementor_Dynamic_Tag_MEC_Event_End_Date());
+}
+add_action('elementor/dynamic_tags/register', 'elmedyta_register_mec_event_dynamic_tags');
+
+/**
+ * Register new dynamic tag group
+ *
+ * @since 1.0.0
+ * @param \Elementor\Core\DynamicTags\Manager $dynamic_tags_manager Elementor dynamic tags manager.
+ * @return void
+ */
+function elmedyta_register_new_dynamic_tag_group($dynamic_tags_manager)
+{
+
+    $dynamic_tags_manager->register_group(
+        'mec-event',
+        [
+            'title' => 'MEC Event'
+        ]
+    );
+}
+add_action('elementor/dynamic_tags/register', 'elmedyta_register_new_dynamic_tag_group');
+
+
+/* Override MEC template to allow native Elementor Template
+    - Needs priority > 99 to work.
+*/
+function elmedyta_override_mec_template($template)
+{
+    if (is_singular('mec-events')) {
+        // Path to the new template file within your plugin directory.
+        $new_template = plugin_dir_path(__FILE__) . 'templates/single-mec-events.php';
+        if (file_exists($new_template)) {
+            return $new_template;
+        }
+    }
+    return $template;
+}
+add_filter('template_include', 'elmedyta_override_mec_template', 101);
